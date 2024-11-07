2c0d85b48f154521c57c0aabd08e06f6ca5b5e81
Muhammet Halak <75750279+halak0013@users.noreply.github.com>
2024-11-07 14:24:24 +0300

    refactor build script for improved package management and automation

--- build.sh
+++ build.sh
@@ -3,37 +3,31 @@
 ### gerekli paketler
 apt install debootstrap xorriso squashfs-tools mtools grub-pc-bin grub-efi-amd64 -y
 
-
 ### Chroot oluşturmak için
 mkdir kaynak
 chown root kaynak
 
-
 ### Ana hiyerarşi oluşturmak için
 debootstrap --arch=amd64 yirmiuc-deb kaynak http://depo.pardus.org.tr/pardus
 
-
 ### bind bağı için
 for i in dev dev/pts proc sys; do mount -o bind /$i kaynak/$i; done
-
 
 sleep 5
 
 echo 'depo ekleniyor...'
 
-
-
 ### Depo eklemek için
-echo 'deb http://depo.pardus.org.tr/pardus yirmiuc-deb main contrib non-free non-free-firmware' > kaynak/etc/apt/sources.list
-echo 'deb-src http://depo.pardus.org.tr/pardus yirmiuc-deb main contrib non-free non-free-firmware' >> kaynak/etc/apt/sources.list
-echo 'deb http://depo.pardus.org.tr/guvenlik yirmiuc-deb main contrib non-free non-free-firmware' >> kaynak/etc/apt/sources.list
-echo 'deb-src http://depo.pardus.org.tr/guvenlik yirmiuc-deb main contrib non-free non-free-firmware' >> kaynak/etc/apt/sources.list
-echo 'deb http://depo.pardus.org.tr/pardus yirmiuc main contrib non-free non-free-firmware' >> kaynak/etc/apt/sources.list
-echo 'deb-src http://depo.pardus.org.tr/pardus yirmiuc main contrib non-free non-free-firmware' >> kaynak/etc/apt/sources.list
+echo 'deb http://depo.pardus.org.tr/pardus yirmiuc-deb main contrib non-free non-free-firmware' >kaynak/etc/apt/sources.list
+echo 'deb-src http://depo.pardus.org.tr/pardus yirmiuc-deb main contrib non-free non-free-firmware' >>kaynak/etc/apt/sources.list
+echo 'deb http://depo.pardus.org.tr/guvenlik yirmiuc-deb main contrib non-free non-free-firmware' >>kaynak/etc/apt/sources.list
+echo 'deb-src http://depo.pardus.org.tr/guvenlik yirmiuc-deb main contrib non-free non-free-firmware' >>kaynak/etc/apt/sources.list
+echo 'deb http://depo.pardus.org.tr/pardus yirmiuc main contrib non-free non-free-firmware' >>kaynak/etc/apt/sources.list
+echo 'deb-src http://depo.pardus.org.tr/pardus yirmiuc main contrib non-free non-free-firmware' >>kaynak/etc/apt/sources.list
 
-echo "deb http://depo.pardus.org.tr/backports yirmiuc-backports main contrib non-free non-free-firmware" >> kaynak/etc/apt/sources.list.d/yirmiuc-backports.list
+echo "deb http://depo.pardus.org.tr/backports yirmiuc-backports main contrib non-free non-free-firmware" >>kaynak/etc/apt/sources.list.d/yirmiuc-backports.list
 
-echo "deb [signed-by=/etc/apt/keyrings/bismih-pubkey.asc arch=$( dpkg --print-architecture )] https://bismih-org.github.io/repository/repo fethan main" >> kaynak/etc/apt/sources.list.d/bismih.list
+echo "deb [signed-by=/etc/apt/keyrings/bismih-pubkey.asc arch=$(dpkg --print-architecture)] https://bismih-org.github.io/repository/repo fethan main" >>kaynak/etc/apt/sources.list.d/bismih.list
 
 ### Anahtar eklemek için
 wget --quiet -O - https://bismih-org.github.io/repository/repo/bismih-pubkey.asc | tee kaynak/etc/apt/keyrings/bismih-pubkey.asc
@@ -43,7 +37,8 @@ wget -c https://depo.pardus.org.tr/pardus/pool/main/p/pardus-archive-keyring/par
 chroot kaynak dpkg -i /tmp/pardus-archive-keyring_2021.1_all.deb
 
 ### ssl olarak githubdaki depo ile bağlantı kurmak için
-chroot kaynak apt install ca-certificate -y
+chroot kaynak apt update -y
+chroot kaynak apt install ca-certificates -y
 
 chroot kaynak apt install --fix-missing -y
 chroot kaynak apt install --fix-broken -y
@@ -51,62 +46,63 @@ chroot kaynak apt install --fix-broken -y
 chroot kaynak apt update -y
 chroot kaynak apt upgrade -y
 
-chroot kaynak apt install -t yirmiuc-backports linux-image-amd64 -y
+chroot kaynak /usr/bin/env DEBIAN_FRONTEND=noninteractive apt install -t yirmiuc-backports linux-image-amd64 -y
+
+chroot kaynak apt install -y locales
+chroot kaynak locale-gen en_US.UTF-8
+chroot kaynak locale-gen tr_TR.UTF-8
+chroot kaynak update-locale LANG=tr_TR.UTF-8
 
 ### grub paketleri için
-chroot kaynak apt install grub-pc-bin grub-efi-ia32-bin grub-efi -y
+chroot kaynak /usr/bin/env DEBIAN_FRONTEND=noninteractive apt install grub-pc-bin grub-efi-ia32-bin grub-efi -y
 
 ### live paketleri için -> dili otomatik seçtir
-chroot kaynak apt install live-config live-boot -y 
+chroot kaynak /usr/bin/env DEBIAN_FRONTEND=noninteractive apt install live-config live-boot -y
+
+echo "firmware-ipw2x00 firmware-ipw2x00/license/accepted select true" | chroot kaynak debconf-set-selections
+echo "firmware-ivtv firmware-ivtv/license/accepted boolean true" | chroot kaynak debconf-set-selections
 
 ### firmware paketleri -> driverlede gen sorulara otomatik kabul ettir
-chroot kaynak apt install bluez-firmware firmware-amd-graphics firmware-atheros firmware-b43-installer firmware-b43legacy-installer firmware-bnx2 firmware-bnx2x firmware-brcm80211 firmware-cavium firmware-intel-sound  firmware-ipw2x00 firmware-ivtv firmware-iwlwifi firmware-libertas firmware-linux firmware-linux-free firmware-linux-nonfree firmware-misc-nonfree firmware-myricom firmware-netxen firmware-qlogic  firmware-realtek firmware-samsung firmware-siano firmware-ti-connectivity firmware-zd1211 firmware-sof-signed zd1211-firmware -y
+chroot kaynak /usr/bin/env DEBIAN_FRONTEND=noninteractive apt install bluez-firmware firmware-amd-graphics firmware-atheros firmware-b43-installer firmware-b43legacy-installer firmware-bnx2 firmware-bnx2x firmware-brcm80211 firmware-cavium firmware-intel-sound firmware-ipw2x00 firmware-ivtv firmware-iwlwifi firmware-libertas firmware-linux firmware-linux-free firmware-linux-nonfree firmware-misc-nonfree firmware-myricom firmware-netxen firmware-qlogic firmware-realtek firmware-samsung firmware-siano firmware-ti-connectivity firmware-zd1211 firmware-sof-signed zd1211-firmware -y
+chroot kaynak update-initramfs -u
 
-# sudo DEBIAN_FRONTEND=noninteractive apt install -y firmware-intel-sound dene
 
 ### kde paketleri
-chroot kaynak apt install kde-standard -y
-
+chroot kaynak /usr/bin/env DEBIAN_FRONTEND=noninteractive apt install kde-standard -y
 
 ###geç kapanma sorunu düzeltme servisi
-echo '#!/bin/sh' > kaynak/usr/lib/systemd/system-shutdown/kill_kwin.shutdown
-echo '# Kill KWin immediately to prevent stalled shutdowns/reboots' >> kaynak/usr/lib/systemd/system-shutdown/kill_kwin.shutdown
-echo 'pkill -KILL kwin_x11' >> kaynak/usr/lib/systemd/system-shutdown/kill_kwin.shutdown
+echo '#!/bin/sh' >kaynak/usr/lib/systemd/system-shutdown/kill_kwin.shutdown
+echo '# Kill KWin immediately to prevent stalled shutdowns/reboots' >>kaynak/usr/lib/systemd/system-shutdown/kill_kwin.shutdown
+echo 'pkill -KILL kwin_x11' >>kaynak/usr/lib/systemd/system-shutdown/kill_kwin.shutdown
 chmod +x kaynak/usr/lib/systemd/system-shutdown/kill_kwin.shutdown
 
-echo '[Unit]' > kaynak/etc/systemd/system/kill_kwin.service
-echo 'Description=Kill KWin at shutdown/reboot' >> kaynak/etc/systemd/system/kill_kwin.service
-echo '' >> kaynak/etc/systemd/system/kill_kwin.service
-echo '[Service]' >> kaynak/etc/systemd/system/kill_kwin.service
-echo 'Type=oneshot' >> kaynak/etc/systemd/system/kill_kwin.service
-echo 'ExecStart=/bin/true' >> kaynak/etc/systemd/system/kill_kwin.service
-echo 'ExecStop=/bin/sh /usr/lib/systemd/system-shutdown/kill_kwin.shutdown' >> kaynak/etc/systemd/system/kill_kwin.service
-echo 'RemainAfterExit=true' >> kaynak/etc/systemd/system/kill_kwin.service
-echo '' >> kaynak/etc/systemd/system/kill_kwin.service
-echo '[Install]' >> kaynak/etc/systemd/system/kill_kwin.service
-echo 'WantedBy=multi-user.target' >> kaynak/etc/systemd/system/kill_kwin.service
+echo '[Unit]' >kaynak/etc/systemd/system/kill_kwin.service
+echo 'Description=Kill KWin at shutdown/reboot' >>kaynak/etc/systemd/system/kill_kwin.service
+echo '' >>kaynak/etc/systemd/system/kill_kwin.service
+echo '[Service]' >>kaynak/etc/systemd/system/kill_kwin.service
+echo 'Type=oneshot' >>kaynak/etc/systemd/system/kill_kwin.service
+echo 'ExecStart=/bin/true' >>kaynak/etc/systemd/system/kill_kwin.service
+echo 'ExecStop=/bin/sh /usr/lib/systemd/system-shutdown/kill_kwin.shutdown' >>kaynak/etc/systemd/system/kill_kwin.service
+echo 'RemainAfterExit=true' >>kaynak/etc/systemd/system/kill_kwin.service
+echo '' >>kaynak/etc/systemd/system/kill_kwin.service
+echo '[Install]' >>kaynak/etc/systemd/system/kill_kwin.service
+echo 'WantedBy=multi-user.target' >>kaynak/etc/systemd/system/kill_kwin.service
 
 chmod +x kaynak/etc/systemd/system/kill_kwin.service
 chroot kaynak systemctl enable kill_kwin.service
 chroot kaynak systemctl start kill_kwin.service
 
-
-chroot kaynak apt purge juk kmail* konqueror kwrite kde-spectacle zutty
+chroot kaynak apt purge juk kmail* konqueror kwrite kde-spectacle zutty -y
 
 ### Yazıcı tarayıcı ve bluetooth paketlerini kuralım (isteğe bağlı)
 chroot kaynak apt install printer-driver-all system-config-printer simple-scan blueman -y
 
-
 ### Pardus paketlerini kurma
-chroot kaynak apt install pardus-about pardus-ayyildiz-grub-theme pardus-backgrounds pardus-font-manager pardus-image-writer pardus-installer pardus-java-installer pardus-locales pardus-menus pardus-mycomputer pardus-night-light pardus-package-installer pardus-software pardus-update pardus-usb-formatter pardus-wallpaper-23-0 git system-monitoring-center -y
+chroot kaynak /usr/bin/env DEBIAN_FRONTEND=noninteractive apt install pardus-about pardus-ayyildiz-grub-theme pardus-backgrounds pardus-font-manager pardus-image-writer pardus-installer pardus-java-installer pardus-locales pardus-menus pardus-mycomputer pardus-night-light pardus-package-installer pardus-software pardus-update pardus-usb-formatter pardus-wallpaper-23-0 git system-monitoring-center -y
 
-
-chroot kaynak apt install bash-completion libreoffice libreoffice-kf5 libreoffice-l10n-tr libreoffice-style-yaru birdtray thunderbird thunderbird-l10n-tr zen-browser touchegg flameshot elisa xsel xdotool unrar webapp-manager appimagelauncher pkg-config -y
+chroot kaynak apt install bash-completion libreoffice libreoffice-kf5 libreoffice-l10n-tr libreoffice-style-yaru birdtray thunderbird thunderbird-l10n-tr touchegg flameshot elisa xsel xdotool unrar webapp-manager appimagelauncher pkg-config -y
 
 ### config ayarları
-
-
-
 
 ### temizlik işlemleri
 chroot kaynak apt upgrade -y
@@ -119,10 +115,8 @@ rm -f kaynak/root/.bash_history
 rm -rf kaynak/var/lib/apt/lists/*
 find kaynak/var/log/ -type f | xargs rm -f
 
-
 ### bind bağını kaldırmak için
 umount -lf -R kaynak/* 2>/dev/null
-
 
 ### isowork filesystem.squashfs oluşturmak için
 mkdir isowork
@@ -135,9 +129,8 @@ mv filesystem.squashfs isowork/live/filesystem.squashfs
 cp -pf kaynak/boot/initrd.img* isowork/live/initrd.img
 cp -pf kaynak/boot/vmlinuz* isowork/live/vmlinuz
 
-### grub işlemleri 
+### grub işlemleri
 mkdir -p isowork/boot
 cp -r grub/ isowork/boot/
-
 
 grub-mkrescue isowork -o bismih-amd64.iso
